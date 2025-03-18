e88736749d27ba56182bb6677eccdb1c0c48862a
Abdullah Khamis <abdullahbkhamis@gmail.com>
2025-03-18 16:04:12 -0400

    added component structure for frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
-import ChartPrediction from "./components/Chart"
+import ChartPrediction from "./components/Chart";
+import Input from "./components/Input";
+import Button from "./components/Button";
+import Analysis from "./components/Analysis";
 
 function App() {
-  return (
-    <>
-        <ChartPrediction />
-    </>
-  );
+	return (
+		<>
+      <Input />
+      <Button />
+      <Analysis />
+			<ChartPrediction />
+		</>
+	);
 }
 
 export default App;
---
+++ client/src/components/Button.jsx
@@ -0,0 +1,7 @@
+export default function Button({duration}) {
+    return (
+        <button>
+            {duration} analysis
+        </button>
+    )
+}
